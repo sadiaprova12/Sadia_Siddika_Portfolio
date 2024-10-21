5c11b10d8117b215a5e17d1475929f37ce9b7148
sadiaprova12 <sadiaprova51717@gmail.com>
2024-10-21 23:03:47 +0600

    added google font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ tailwind.config = {
     theme:{
         gridTemplateColumns:{
             'auto': 'repeat(auto-fit, minmax(200px, 1fr))'
+        },
+        fontFamily:{
+            Outfit:["Outfit", "sans-serif"],
+            Ovo: ["Ovo", "serif"]
         }
     }
 }
